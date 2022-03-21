500ea5a876f191ebe4f05d9e2ace50b976d5c6f7
Guilherme Suzuki <suzuki.gui@gmail.com>
2022-03-21 17:19:23 -0300

    Added EFCore6 for MySQL.

---
+++ EFCore6_MySQL/Models/LoadTestContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore6_MySQL.Models
+{
+    public class LoadTestContext: DbContext
+    {
+        public LoadTestContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //options.UseSqlServer("Server=localhost;Database=LoadTest_EFCore6;User Id=loadtest;Password=loadtest;");
+            options.UseMySQL("Server=localhost;Database=LoadTest_EFCore6;User Id=loadtest;Password=~Fe6#Sh1;");
+            base.OnConfiguring(options);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasMany(x => x.KnownForTitles)
+                .WithOne()
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasForeignKey(x => x.NConst);
+
+            modelBuilder.Entity<Person>()
+                .HasMany(x => x.PrimaryProfession)
+                .WithOne()
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasForeignKey(x => x.NConst);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<Profession> Professions { get; set;}
+    }
+}
---
+++ EFCore6_MySQL/Models/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore6_MySQL.Models
+{
+    [Table("People")]
+    public class Person
+    {
+        /// <summary>
+        /// alphanumeric unique identifier of the name/person
+        /// </summary>
+        [Key]
+        public string NConst { get; set; } = string.Empty;
+        /// <summary>
+        /// name by which the person is most often credited
+        /// </summary>
+        [Column]
+        public string PrimaryName { get; set; } = string.Empty;
+        /// <summary>
+        /// in YYYY format
+        /// </summary>
+        [Column]
+        public short? BirthYear { get; set; }
+        /// <summary>
+        /// in YYYY format if applicable, else '\N'
+        /// </summary>
+        [Column]
+        public short? DeathYear { get; set; }
+        /// <summary>
+        /// (array of strings)– the top-3 professions of the person
+        /// </summary>
+        [Column]
+        public List<Profession> PrimaryProfession { get; set; } = new List<Profession>();
+        /// <summary>
+        /// (array of tconsts) – titles the person is known for
+        /// </summary>
+        public List<Title> KnownForTitles { get; set; } = new List<Title>();
+        /// <summary>
+        /// Column created especially for Updates
+        /// </summary>
+        [Column]
+        public string ColumnForUpdateTest { get; set; } = string.Empty;
+    }
+}
